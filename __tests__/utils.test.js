7a12b86a0573c7bcd4bdbc15114fbedfb4f18f6d
Leonardo <josue.aguilar1995@gmail.com>
2022-07-26 17:52:30 -0500

    added falsy test for email validation;

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -7,8 +7,14 @@ test("reformats current date", () => {
   expect(dateFormat(date)).toBe("Jul 25th, 2022 at 13:47 pm");
 });
 
-test("checks for email validation", () => {
-  const email = "person@email.com";
+test("checks for positive email validation", () => {
+  const email = "an@email.com";
 
   expect(validateEmail(email)).toBeTruthy();
+});
+
+test("checks for negative email validation", () => {
+  const email = "not an email";
+
+  expect(validateEmail(email)).toBeFalsy();
 });
