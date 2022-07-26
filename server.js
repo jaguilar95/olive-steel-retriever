1240bad9525a2eeee66995637f09355fe9fe7ed9
Leonardo <josue.aguilar1995@gmail.com>
2022-07-26 16:59:29 -0500

    started server set up

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// app.use(require("./routes"));
+
+mongoose.connect("mongodb://localhost:27017/olive-steel-retriever", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// log mongo queries being executed
+mongoose.set("debug", true);
+
+app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
