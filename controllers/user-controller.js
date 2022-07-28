6b2b9e58a5b0bab284a2cea49d17c3ca5e505682
Leonardo <josue.aguilar1995@gmail.com>
2022-07-27 21:14:33 -0500

    finished friend routes

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -42,6 +42,25 @@ const userController = {
       .catch((err) => res.status(400).json(err));
   },
 
+  // add a friend with id and friendId
+  addFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.id },
+      { $push: { friends: params.friendId } },
+      { new: true, runValidators: true }
+    )
+      .then((dbUserData) => {
+        // if no user data is found, send 404
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
+
   // update a user by id
   updateUser({ params, body }, res) {
     User.findOneAndUpdate({ _id: params.id }, body, {
@@ -73,6 +92,17 @@ const userController = {
         res.json(dbUserData);
       })
       .catch((err) => res.status(400).json(err));
+  },
+
+  // remove a friend by id and friendId
+  removeFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.id },
+      { $pull: { friends: params.friendId } },
+      { new: true }
+    )
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.json(err));
   },
 };
 
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,8 +4,10 @@ const {
   getAllUsers,
   getUserById,
   createUser,
+  addFriend,
   updateUser,
   deleteUser,
+  removeFriend,
 } = require("../../controllers/user-controller");
 
 // set up /api/users
@@ -13,5 +15,8 @@ router.route("/").get(getAllUsers).post(createUser);
 
 // set up /api/users/:id
 router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
+
+// set up /api/users/:id/:friendId
+router.route("/:id/:friendId").post(addFriend).delete(removeFriend);
 
 module.exports = router;
