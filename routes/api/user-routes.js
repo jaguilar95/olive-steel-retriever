2ba545bf2125c7b14b9761ea1985a46926fde4be
Leonardo <josue.aguilar1995@gmail.com>
2022-07-27 20:00:54 -0500

    added initial user controllers & routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -2,10 +2,16 @@ const express = require("express");
 const router = express.Router();
 const {
   getAllUsers,
+  getUserById,
   createUser,
+  updateUser,
+  deleteUser,
 } = require("../../controllers/user-controller");
 
 // set up /api/users
 router.route("/").get(getAllUsers).post(createUser);
+
+// set up /api/users/:id
+router.route("/:id").get(getUserById).put(updateUser).delete(deleteUser);
 
 module.exports = router;
